47354a403e0b24939232041dfb8a081150bb4c60
Ahmad Aminullah Alfiyanto <aminullah.alfiyanto@gmail.com>
2025-02-15 15:10:54 +0700

    initial commit

---
+++ catgallerybe/src/main/java/com/example/catgallerybe/CatgallerybeApplication.java
@@ -0,0 +1,13 @@
+package com.example.catgallerybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatgallerybeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatgallerybeApplication.class, args);
+	}
+
+}
---
+++ catgallerybe/src/test/java/com/example/catgallerybe/CatgallerybeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catgallerybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatgallerybeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
