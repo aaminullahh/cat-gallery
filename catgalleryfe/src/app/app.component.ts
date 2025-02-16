cf00c98dd21e717e05b264540be5b852f16834c0
Ahmad Aminullah Alfiyanto <aminullah.alfiyanto@gmail.com>
2025-02-16 20:25:49 +0700

    feat: create cat gallery component

--- catgalleryfe/src/app/app.component.ts
+++ catgalleryfe/src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'catgalleryfe';
+export class AppComponent implements OnInit {
+  // title = 'catgalleryfe';
+  cats: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchCats();
+  }
+
+  fetchCats(): void {
+    this.http.get<any[]>('http://localhost:8080/cats').subscribe(data => {
+      this.cats = data;
+    });
+  }
+
+  uploadImage(event: any): void {
+    const file = event.target.files[0];
+    if (!file) return;
+    
+    const formData = new FormData();
+    formData.append('file', file);
+    
+    this.http.post('http://localhost:8080/cats/upload', formData).subscribe(() => {
+      this.fetchCats();
+    });
+  }
+
+  deleteCat(id: string): void {
+    this.http.delete(`http://localhost:8080/cats/${id}`).subscribe(() => {
+      this.fetchCats();
+    });
+  }
 }
